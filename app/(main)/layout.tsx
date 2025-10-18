625fe8e1d0354d9ec5a96f9894a85ac406199857
realshak7781 <124501803+realshak7781@users.noreply.github.com>
2025-10-19 01:22:47 +0530

    changes made to the landingPage

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,24 @@
+import PublicNavBar from "@/components/PublicNavBar"
+import { currentUser } from "@clerk/nextjs/server"
+
+export default async function MainLayout({
+    children,
+} : {
+    children : React.ReactNode
+}){
+
+    const user= await currentUser()
+
+    return <main className="relative">
+
+        <PublicNavBar/>
+        {/* {render  a privateNav bar or a public Navbar} */}
+
+        {/* {user ? <PrivateNavBar/> : <PublicNavBar/>} */}
+        {/* {render the children} */}
+        <section className="pt-36">
+            {children}
+        </section>
+
+    </main>
+}
---
+++ components/PublicNavBar.tsx
@@ -0,0 +1,10 @@
+
+export default function PublicNavBar(){
+
+    return(
+        <nav className="flex justify-between items-center fixed z-50 w-full h-28 bg-gray-300 px-10 gap-4 shadow-2xl">
+            
+
+        </nav>
+    )
+}
